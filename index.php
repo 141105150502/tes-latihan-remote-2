ef5c405734aef90413ada945bf6e7693c89a8f0f
141105150502 <dikygayahachiman22@gmail.com>
2020-11-13 06:14:32 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 	<h1>git latihan</h1>
+	<p>Lorem ipsum, dolor sit amet consectetur, adipisicing elit. Laboriosam, in ipsam sunt non dignissimos soluta illum ratione facere culpa delectus corporis at minus perspiciatis dolorem nobis dolorum sequi deleniti necessitatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, cupiditate, nihil quod tempore suscipit tempora nostrum facere atque, voluptatem beatae assumenda dolores, pariatur quas earum consequatur modi eaque impedit sapiente?</p>
 	
 </body>
 </html>
